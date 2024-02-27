9413fd967d8418e782703990b7a0a1c9f391fe7b
cje206 <154851961+cje206@users.noreply.github.com>
2024-02-27 09:49:34 +0900

    DB연결 확인

--- config/config.js
+++ config/config.js
@@ -17,10 +17,11 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
+        username: process.env.PRODNAME,
+        password: process.env.PRODPW,
+        database: process.env.PRODDATA,
+        host: process.env.PRODHOST,
         dialect: 'mysql',
+        port: process.env.PORT,
     },
 };
