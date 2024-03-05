77ff68d6cc972b5a7a1283eb626047ead07f3409
Jihye8 <brenda829@naver.com>
2024-03-05 13:53:21 +0900

    tripdetail 지도 api 수정중...

--- public/js/tripdetail.js
+++ public/js/tripdetail.js
@@ -31,12 +31,13 @@ const list = document.querySelector('.container_tripdetail');
             const { category, arrTime, detailMemo, groupId, distance, place } =
                 res.data.result[i];
             const showTime = arrTime.substring(0, 5);
-            // console.log(showTime);
             if (j === category - 1) {
                 var hour = (distance * 4) / 60;
                 var min = (distance * 4) % 60;
                 html = `
-                <li class="list_box" onclick="openBox(${j}, ${k})">
+                <li class="list_box" onclick="openBox(${j}, ${k},${place.x},${
+                    place.y
+                })">
                     <div class="list_detail">
                         <strong>${place.place_name}</strong>
                         <span>${showTime}</span>
@@ -62,39 +63,85 @@ function goChecklist() {
 function goEdit() {
     document.location.href = '/calendar';
 }
+
+let openMaps = []; // 열린 지도의 배열
 function openCategory(list) {
-    const nowList = document.querySelectorAll('.detail_wrap')[list];
-    if (nowList.classList.contains('open')) {
-        nowList.classList.remove('open');
-    } else {
-        nowList.classList.add('open');
+    const allLists = document.querySelectorAll('.detail_wrap');
+    allLists.forEach((listElement, index) => {
+        if (index === list) {
+            listElement.classList.toggle('open');
+        }
+    });
+    // 현재 열린 지도 삭제
+    if (openMaps[list]) {
+        console.log(openBox[list].box);
+        removeMap(list, openMaps[list].box);
+        openMaps[list] = null;
     }
 }
-function openBox(list, box) {
+function openBox(list, box, x, y) {
+    if (openMaps[list]) {
+        const allLists = document.querySelectorAll('.detail_wrap');
+        allLists.forEach((listElement, index) => {
+            if (index !== list) {
+                listElement.classList.toggle('open');
+            }
+        });
+        removeMap(list, openMaps[list].box);
+        openMaps[list] = null;
+    }
     const nowList = document.querySelectorAll('.detail_wrap')[list];
     const nowBox = nowList.querySelectorAll('.list_box')[box];
-    const count = document.querySelectorAll('.list_box').length;
-    for (let i = 0; i < count; i++) {
-        if (!nowBox === document.querySelectorAll('.list_box')[i]) {
-            document.querySelectorAll('.list_box')[i].classList.remove('on');
-        }
-    }
-    if (nowBox.classList.contains('on')) {
-        nowBox.classList.remove('on');
-        console.log(nowBox);
-    } else {
+
+    // 현재 클릭한 리스트의 지도 열기
+    if (!nowBox.classList.contains('on')) {
         nowBox.classList.add('on');
+        openMaps[list] = { box, x, y }; // 열린 지도의 정보 저장
+
+        console.log('maps add:', list, openMaps);
+        openMap(list, box, x, y);
+    } else {
+        nowBox.classList.remove('on');
     }
-    openMap(list, box);
+}
+function openMap(list, box, place_x, place_y) {
+    var place_x = place_x;
+    var place_y = place_y;
+    const nowList = document.querySelectorAll('.detail_wrap')[list];
+    const nowBox = nowList.querySelectorAll('.list_box')[box];
+    if (nowBox.classList.contains('on')) {
+        html = `<div id="map" style="width:400px;height:200px;"></div>`;
+        nowBox.insertAdjacentHTML('beforeend', html);
+    }
+    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    //지도를 생성할 때 필요한 기본 옵션
+    var options = {
+        center: new kakao.maps.LatLng(place_y, place_x), //지도의 중심좌표.
+        level: 2, //지도의 레벨(확대, 축소 정도)
+    };
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    var markerPosition = new kakao.maps.LatLng(place_y, place_x);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition,
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
 }
 
-function openMap(list, box) {
+function removeMap(list, box) {
     const nowList = document.querySelectorAll('.detail_wrap')[list];
     const nowBox = nowList.querySelectorAll('.list_box')[box];
-    console.log(nowList);
-    if (nowBox.classList.contains('on')) {
-        console.log('성공');
-        html = `<div style="height : 100px">지도</div>`;
-        nowBox.insertAdjacentHTML('beforeend', html);
+    console.log('List', nowList);
+    console.log('Box', nowBox);
+
+    // 현재 열린 지도가 있는 경우에만 삭제
+    if (nowBox && nowBox.classList.contains('on')) {
+        var mapElement = nowBox.querySelector('#map');
+        if (mapElement) {
+            mapElement.parentNode.removeChild(mapElement);
+        }
     }
 }
