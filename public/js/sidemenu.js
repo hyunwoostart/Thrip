3dc58e56622962420d2119fbc9cef5665a258875
cje206 <projec206@gmail.com>
2024-03-05 15:48:51 +0900

    fix: 메인페이지 스크립트 파일 분리

--- public/js/sidemenu.js
+++ public/js/sidemenu.js
@@ -2,6 +2,51 @@ const hamBtn = document.querySelector('.ico_ham');
 const menuSide = document.querySelector('.menu_side');
 const closeBtn = document.querySelector('.btn_close');
 
+// 사용자 인증
+(async function () {
+    const info = document.querySelector('.my_info');
+    const myName = document.querySelector('.menu_side .info_txt');
+    const editBtn = document.querySelector('.btn_retouch');
+    const logoutBtn = document.querySelector('.btn_logout');
+    if (!localStorage.getItem('token')) {
+        editBtn.hidden = true;
+        logoutBtn.hidden = true;
+        info.addEventListener('click', () => {
+            document.location.href = '/login';
+        });
+    } else {
+        try {
+            const res = await axios({
+                method: 'GET',
+                url: '/api/member/find',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+            editBtn.hidden = false;
+            logoutBtn.hidden = false;
+            myName.textContent = res.data.result.username;
+        } catch (error) {
+            editBtn.hidden = true;
+            localStorage.clear();
+            info.addEventListener('click', () => {
+                document.location.href = '/login';
+            });
+        }
+    }
+})();
+
+// 개인정보 수정
+function editFunc() {
+    document.location.href = '/myPage';
+}
+
+// 로그아웃
+function logoutFunc() {
+    localStorage.clear();
+    document.location.reload();
+}
+
 hamBtn.addEventListener('click', () => {
     event.preventDefault();
     menuSide.style.display = 'block';
