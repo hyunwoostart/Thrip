f1b85979c088e865c24e57d641a11614170bc373
hwji <knjhw@daum.net>
2024-03-06 11:01:05 +0900

    trip 페이지 수정 v1

--- public/js/sidemenu.js
+++ public/js/sidemenu.js
@@ -51,21 +51,18 @@ hamBtn.addEventListener('click', () => {
     event.preventDefault();
     menuSide.style.display = 'block';
     document.body.style.overflow = 'hidden';
-    // menuSide.classList.add('dimed');
 });
 
 closeBtn.addEventListener('click', () => {
     event.preventDefault();
     menuSide.style.display = 'none';
     document.body.style.overflow = 'auto';
-    // menuSide.classList.remove('dimed');
 });
 
 // 모달 바깥 영역 클릭 시 모달 닫기
 document.addEventListener('click', (event) => {
     if (!menuSide.contains(event.target) && event.target !== hamBtn) {
         menuSide.style.display = 'none';
-        // document.body.classList.remove('dimed'); // 딤처리 클래스 제거
     }
 });
 
